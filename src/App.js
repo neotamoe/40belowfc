126235b2d06ac8d0f0116e6b61ba60d1cf2a56e8
Neota Moe <neota.moe@gmail.com>
2019-06-15 09:30:13 -0500

    tweaks to main page; added react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,21 +5,30 @@ function App() {
   return (
     <div>
       <div className="App">
-      <img src="/assets/images/crest2stars.png" alt="team crest"></img>
+        <img src="/assets/images/crest2stars.png" alt="team crest"></img>
         <h1>40 Below FC</h1>
       </div>
-    <div>
-      <h3>PLANNING:</h3>
-      <ul>
-        <li>navbar</li>
-        <li>current roster</li>
-        <li>former roster</li>
-        <li>stats</li>
-        <li>next game</li>
-        <li>photos</li>
-        <li>admin page</li>
+
+      <ul className="header">
+        <li><a href="/">Home</a></li>
+        <li><a href="/stuff">Stuff</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
-    </div>
+      <div className="content" style={{border: 'blue 1px solid'}}>
+          <p>Content to go here</p>
+      </div>
+      <div style={{border: 'purple 1px solid', marginTop: '50px'}}>
+        <h3>PLANNING (this will disappear later):</h3>
+        <ul>
+          <li>navbar</li>
+          <li>current roster</li>
+          <li>former roster</li>
+          <li>stats</li>
+          <li>next game</li>
+          <li>photos</li>
+          <li>admin page</li>
+        </ul>
+      </div>
     </div>
   );
 }
