1ff786b913ec2a0ec236e90fbadc6b08c893877d
Neota Moe <neota.moe@gmail.com>
2019-06-14 19:56:39 -0500

    starter app for 40 below

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+      <img src="/assets/images/crest2stars.png" alt="team crest"></img>
+        <h1>40 Below FC</h1>
+      </div>
+    <div>
+      <h3>PLANNING:</h3>
+      <ul>
+        <li>navbar</li>
+        <li>current roster</li>
+        <li>former roster</li>
+        <li>stats</li>
+        <li>next game</li>
+        <li>photos</li>
+        <li>admin page</li>
+      </ul>
+    </div>
     </div>
   );
 }
