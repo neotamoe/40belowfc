506549c663d359eba6ba54851f237b15ede69356
Neota Moe <neota.moe@gmail.com>
2019-06-25 22:20:26 -0500

    add fetch to local backend

--- src/Roster.js
+++ src/Roster.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Roster() {
-  return (
-    <div>
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>#</th>
-                <th>Position</th>
-            </tr>
-            <tr>
-                <td>Neota Moe</td>
-                <td>31</td>
-                <td>Defense</td>
-            </tr>
-        </table>
+class Roster extends Component {
+    state = {
+        players: []
+    }
+    componentWillMount(){
+        fetch('http://localhost:8080/players')
+            .then(response => response.json()
+            .then(res => {
+                // console.log(res)
+                this.setState({
+                    players: res
+                })
+            }))
+    }
 
-    </div>
-  );
+    render() {
+        const players = this.state.players;
+
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {players.map(player => 
+                            <tr key={player.id}>
+                                <td>{player.first_name}</td>
+                                <td>{player.last_name}</td>
+                                <td>{player.jersey_number}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default Roster;
