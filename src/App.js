689ffa5e172932b24857ca60b9a9e2a6c34992fc
Neota Moe <neota.moe@gmail.com>
2019-07-07 09:11:23 -0500

    clean up css and navbar display

--- src/App.js
+++ src/App.js
@@ -8,20 +8,17 @@ import Stats from './Stats';
 function App() {
   return (
     <div>
-      <div id="myHeading">
+      <div id="main-heading">
         <img src="/assets/images/crest2stars.png" alt="team crest"></img>
         <h1 className="App" >40 Below FC</h1>
-      </div>
-      <div>
-
-      <navbar>
-        <ul className="header">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/roster">Current Roster</Link></li>
-          <li><Link to="/roster/former">Former Roster</Link></li>
-          <li><Link to="/stats">Stats</Link></li>
-        </ul>
-      </navbar>
+        <navbar>
+          <ul className="header">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/roster">Current Roster</Link></li>
+            <li><Link to="/roster/former">Former Roster</Link></li>
+            <li><Link to="/stats">Stats</Link></li>
+          </ul>
+        </navbar>      
       </div>
       <div className="content">
         <Switch>
