3e66d2e11a6437864273deb2219395e9248a7d54
Neota Moe <neota.moe@gmail.com>
2019-07-15 21:26:12 -0500

    generating react-data-grid table for all except gamees

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import ReactDataGrid from 'react-data-grid';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class Admin extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            type: '0'
+            type: '0',
+            formFields: null,
+            data: null,
         };
     
         this.handleChange = this.handleChange.bind(this);
@@ -18,23 +22,50 @@ class Admin extends Component {
     handleSubmit(event) {
         alert('A data type was submitted: ' + this.state.type);
         event.preventDefault();
+        fetch('http://localhost:8080/' + this.state.type)
+            .then(response =>  response.json())
+            .then(results => {
+                console.log(results)
+                console.log(Object.keys(results[0]))
+                this.setState({
+                    formFields: Object.keys(results[0]),
+                    data: results
+                })
+            });
     }
 
     render() {
+        const columns = this.state.formFields ?
+        this.state.formFields.map(field => ({ key: field, name: field })) : [];
+        // [
+        //     { key: 'id', name: 'ID' },
+        //     { key: 'title', name: 'Title' },
+        //     { key: 'count', name: 'Count' } ];
+          
+        const rows = this.state.data ? this.state.data : [];
+        console.log("data.length: ", this.state.data ? this.state.data.length : "none")
+        const rowsCount = this.state.data ? this.state.data.length : 0;
+        const dataGrid = (this.state.formFields && this.state.data) ? <div className="admin-grid"><ReactDataGrid
+            columns={columns}
+            rowGetter={i => rows[i]}
+            rowsCount={rowsCount}
+            minHeight={150} /></div> : null;
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="datalist">Select Data Type</label>
                     <select value={this.state.type} name="datalist" id="datalist" form="" onChange={this.handleChange}>
                         <option value='0' disabled>Select Data Type</option>
-                        <option value="Location">Location</option>
-                        <option value="Game">Game</option>
-                        <option value="Player">Player</option>
-                        <option value="Opponent">Opponent</option>
-                        <option value="Season">Season</option>
+                        <option value="locations">Location</option>
+                        <option value="games">Game</option>
+                        <option value="players">Player</option>
+                        <option value="opponents">Opponent</option>
+                        <option value="seasons">Season</option>
                     </select><br />    
                     <input type="submit" />        
                 </form>
+                <br />
+                {dataGrid}
             </div>
         );
   }
