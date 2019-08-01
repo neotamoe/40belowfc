fae683b97a65ed1efe42aa0ae57df81aaf53a793
Neota Moe <neota.moe@gmail.com>
2019-07-31 22:36:00 -0500

    add error handling to FormerRoster

--- src/components/FormerRoster.js
+++ src/components/FormerRoster.js
@@ -5,18 +5,30 @@ import Spinner from './Spinner';
 class FormerRoster extends Component {
     state = {
         players: [],
-        isLoading: true
+        isLoading: true,
+        error: false
     }
     componentWillMount(){
         fetch('http://localhost:8080/players/former')
-            .then(response => response.json()
+            .then((response) => {
+                if(response.ok) {
+                    return response;
+                }
+                throw Error(response.statusText)
+            })
             .then(res => {
                 let playersSorted = res.sort((a, b) => { return (a.last_name < b.last_name) ? -1 : ((a.last_name > b.last_name) ? 1 : 0) });
                 this.setState({
                     players: playersSorted,
                     isLoading: false
                 })
-            }))
+            })
+            .catch((error) => {
+                this.setState({
+                    error: true,
+                    isLoading: false
+                })
+            })
     }
 
     render() {
@@ -24,7 +36,7 @@ class FormerRoster extends Component {
 
         return (
             <div>
-                { this.state.isLoading ? <Spinner/> : table }
+                { this.state.isLoading ? <Spinner/> : this.state.error ? <p>Uh oh.  Something went wrong.  Try again.</p> : table }
             </div>
         );
     }
